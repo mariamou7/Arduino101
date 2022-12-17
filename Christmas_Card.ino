1be61e7f584e78b247512db18561728bfe09a136
Maria-Anastasia Moustaka <mariamou7@windowslive.com>
2022-12-17 10:59:13 +0200

    Update Christmas_Card.ino

--- Christmas_Card.ino
+++ Christmas_Card.ino
@@ -4,13 +4,15 @@
  */
 
 // These constants won't change:
-const int buttonPin = 2;    // pin that the sensor is attached to
-const int buzzerPin = 8;
-const int redLedPin = 9;
-const int greenLedPin = 10;
+//christmas lights
+const int LED_GREEN = 2;
+const int LED_ORANGE = 7;
+const int LED_RED = 10;
+const int BUTTON = 5;
+const int BUZZER = 9;
 
-// variables:
-int buttonState = 1;
+int buttonState = 0;
+
 int tempo = 200;
 char notes[] = "eeeeeeegcde fffffeeeeddedg";
 int duration[] = {1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
@@ -21,53 +23,59 @@ void playTheTone(char note, int duration) {
 
   for (int i = 0; i <= sizeof(tones); i++) {
     if (note == notesName[i]) {
-      tone(buzzerPin, tones[i], duration);
+      tone(BUZZER, tones[i], duration);
     }
   }
 }
 
-//the setup function runs once when you press reset or power the board
 void setup()
 {
-  pinMode(buttonPin, INPUT); //initialize "button" as an input
-  pinMode(redLedPin, OUTPUT); //initialize "led" as an output
-  pinMode(greenLedPin, OUTPUT); //initialize "led" as an output
-  pinMode(buzzerPin, OUTPUT); //initialize "buzzer" as an output
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_ORANGE, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  pinMode(BUZZER, OUTPUT);
 }
 
-//the loop function runs over and over again forever
 void loop()
 {
-  buttonState = digitalRead(buttonPin); //read if button is pressed
+  buttonState = digitalRead(BUTTON);
 
-  //if button is pressed
-  if(buttonState == LOW)
-  {
-    // Jingle Bells
-    for (int i = 0; i <= sizeof(notes)-1; i++){
+  if (buttonState == HIGH){
+
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_ORANGE, HIGH);
+    digitalWrite(LED_RED, HIGH);
+
+    for (int i = 0; i <= sizeof(notes)-1; i++) {
     if (notes[i] == ' ') {
       delay(duration[i] * tempo);
-      
-      digitalWrite(redLedPin, HIGH); //light the led
-      digitalWrite(greenLedPin, LOW); //light the led
-      delay(100);
-      digitalWrite(redLedPin, LOW); //turn off the led
-      digitalWrite(greenLedPin, HIGH); //light the led
-      delay(100);
+
+      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(LED_ORANGE, LOW);
+      digitalWrite(LED_RED, HIGH);
+
+
     } else {
       playTheTone(notes[i], duration[i] * tempo);
+
+      digitalWrite(LED_ORANGE, HIGH);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, HIGH);
       
-      digitalWrite(redLedPin, HIGH); //light the led
-      digitalWrite(greenLedPin, LOW); //light the led
       delay(100);
-      digitalWrite(redLedPin, LOW); //turn off the led
-      digitalWrite(greenLedPin, HIGH); //light the led
-      delay(100);
+
+      digitalWrite(LED_ORANGE, LOW);
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+  
     }
     delay((tempo*2)*duration[i]);
-  	}
 
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_ORANGE, LOW);
+    digitalWrite(LED_RED, HIGH);
 
-    int buttonState = 1; //assigned the value 1 to the "buttonState" variable, as the button is pressed again
   }
+  } 
 }
